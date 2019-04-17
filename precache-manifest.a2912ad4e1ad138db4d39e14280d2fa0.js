2834841a844147a54390a7f4d019f994c6fdaa2a
nikhil karkra <karkra.nikhil@gmail.com>
2019-04-17 22:56:51 +1000

    Updates

---
+++ precache-manifest.a2912ad4e1ad138db4d39e14280d2fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/repository-name/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "e42baf1f51de1a32d3e8",
+    "url": "/repository-name/static/js/main.50f79b7b.chunk.js"
+  },
+  {
+    "revision": "cc38440f43d8263273cb",
+    "url": "/repository-name/static/js/2.560b0043.chunk.js"
+  },
+  {
+    "revision": "e42baf1f51de1a32d3e8",
+    "url": "/repository-name/static/css/main.b7d612da.chunk.css"
+  },
+  {
+    "revision": "6b0164e581b48f4f0cdbf8a9b5f89298",
+    "url": "/repository-name/index.html"
+  }
+];
