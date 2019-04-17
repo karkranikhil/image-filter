088b818b763c007d193ad23991921ced9f26b3c6
nikhil karkra <karkra.nikhil@gmail.com>
2019-04-17 22:55:28 +1000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Container, Image} from './style.js'
+const INITIAL={
+  blur:'0',
+  greyscale:'0',
+  brightness:'1',
+  contrast:'1',
+  hue:'0',
+  saturate:'1',
+  opacity:'1',
+  sepia:'0',
+}
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     ...INITIAL,
+      url: 'https://d27k8xmh3cuzik.cloudfront.net/wp-content/uploads/2018/03/acj-2003-beautiful-landscapes-around-the-world-4.jpg'
+    };
+  }
+  changeValue= (event)=>{
+    this.setState({
+      [event.target.name]:event.target.value
+    })
+    
+  }
+  readURL=(event)=> {
+    let input = event.target
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload =  (e)=> {
+                  this.setState({url:e.target.result, ...INITIAL})
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <>
+      <div className="Navbar">
+        <div>CSS Filter Demo with react and Styled components</div>
+          <div>
+          <label htmlFor="imageUpload" className="uploadBtn">Try your photo</label>
+          <input type='file' onChange={this.readURL} id="imageUpload"/>
+        </div>
       </div>
+      
+        
+      <Container>
+      
+      <Image id="blah"
+       blur={this.state.blur}
+       greyscale={this.state.greyscale}
+       brightness={this.state.brightness}
+       contrast={this.state.contrast} 
+       hue={this.state.hue} 
+       opacity={this.state.opacity} 
+       saturate={this.state.saturate}
+       sepia={this.state.sepia} 
+       src={this.state.url} alt="your image" />
+       <div className="width100">
+       <h2 className="textcolor padding15 heading">Customize Your Image</h2>
+        <Slider name="blur" value={this.state.blur} min="0" max="10" callback={()=>this.changeValue}/>
+        <Slider name="greyscale" value={this.state.greyscale} min="0" max="10" callback={()=>this.changeValue}/>
+        <Slider name="brightness" value={this.state.brightness} min="0" max="3" step="0.1" callback={()=>this.changeValue}/>
+        <Slider name="contrast" value={this.state.contrast} min="1" max="10" callback={()=>this.changeValue} />
+        <Slider name="hue" value={this.state.hue} min="1" max="100" callback={()=>this.changeValue}/>
+        <Slider name="opacity" value={this.state.opacity} min="0" max="1" callback={()=>this.changeValue} step="0.1"/>
+        <Slider name="saturate" value={this.state.saturate} min="1" max="10" callback={()=>this.changeValue}/>
+        <Slider name="sepia" value={this.state.sepia} min="0" max="10" callback={()=>this.changeValue}/>
+       </div>
+      
+      </Container>
+      </>
     );
   }
 }
-
+export const Slider=(props)=>{
+  return (
+      <div className="slidecontainer">
+        <div className="width15 filterStyle">{props.name}:</div> 
+        <div className="width80"><input type="range" min={props.min} max={props.max} value={props.value} step={props.step || 1} className="slider" name={props.name} id={props.name} onChange={props.callback()}/></div>
+      </div>
+  )
+}
 export default App;
